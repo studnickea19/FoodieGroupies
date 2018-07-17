8e5415917890948eed644a2d7cde5b871b779cfe
Ellie Studnicka <e.studnicka@gmail.com>
2018-07-17 11:02:42 -0500

    Inital Commit

---
+++ FoodieGroupies/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodieGroupies.MvcApplication" Language="C#" %>
---
+++ FoodieGroupies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodieGroupies.Startup))]
+namespace FoodieGroupies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
